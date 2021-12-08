410aa167a2dd54a17d0722ae5eb20ee934da9dfa
mandrade <marcos.lgxpower@gamil.com>
2021-12-08 16:57:08 -0300

    Initial commit

---
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.example.motivation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        if(supportActionBar != null){
+            supportActionBar!!.hide()
+        }
+
+        btnSalvar.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if (id == R.id.btnSalvar){
+            salvar()
+        }
+    }
+    private fun salvar(){
+        val nome = txtNome.text.toString()
+        if(nome != ""){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }else{
+            Toast.makeText(this,"Informe seu nome!",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
